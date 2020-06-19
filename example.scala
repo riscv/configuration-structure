733c8e1423250e7780eee8f46fc69aea68b65a06
Tim Newsome <tim@sifive.com>
2020-06-19 13:44:28 -0700

    Expanded example a little.

--- example.scala
+++ example.scala
@@ -3,10 +3,12 @@
 // javax/tools/DiagnosticListener which is an artifact of not using the "right"
 // java version.
 
+// value/mask tuples or low/high/mask triples to describe possible values.
 trait PossibleValues
 case class Tuple(var value: Int, var mask: Int) extends PossibleValues
 case class Triple(var low: Int, var high: Int, var mask: Int) extends PossibleValues
 
+// low-high range to describe possible values
 case class CsRange(val low: Int, val high: Int)
 implicit class Single(val value: Int) extends CsRange(value, value) {
     def --(that: Single) = CsRange(this.value, that.value)
@@ -18,14 +20,12 @@ object Ranges {
     def apply(ranges: CsRange *) = new Ranges(ranges.toList)
 }
 
-trait GenericEntry extends DebugEntry
-        with IsaEntry
-        with PrivEntry
-        with DebugModuleEntry
-        with FastIntEntry
+trait GenericEntry
 trait Context extends GenericEntry
 trait Category extends GenericEntry
-case class Hart(val ranges:Ranges, entries: GenericEntry*) extends Context
+case class Hart(val ranges:Ranges, entries: Category*) extends Context
+
+case class Description(val entries: GenericEntry*)
 
 // Debug info pertaining to a hart.
 trait DebugEntry
@@ -54,14 +54,21 @@ case class PrivCategory(entries: PrivEntry*) extends Category
 trait FastIntEntry
 case class FastIntMachineModeTimeRegAddr(val address:Number) extends FastIntEntry
 case class FastIntMachineModeTimeCompareRegAddr(val address:Number) extends FastIntEntry
-case class FastIntCategory(entries: FastIntEntry*)
+case class FastIntCategory(entries: FastIntEntry*) extends Category
+
+// Trace stuff
+trait TraceEntry
+case class TraceBranchPredictorEntries(val entries:Number) extends TraceEntry
+case class TraceJumpTargetCacheEntries(val entries:Number) extends TraceEntry
+case class TraceContextBusWidth(val width:Number) extends TraceEntry
+case class TraceCategory(entries: TraceEntry*) extends Category
 
 val LOW = 0x1234
 val HIGH = 0x5678
 val MASK = 0xff00
 val VALUE = LOW
 
-var description = List(
+var description = Description(
     Hart(Ranges(0),
         DebugCategory(
             DebugTrigger(Ranges(0 -- 3),
@@ -106,28 +113,23 @@ var description = List(
         ),
         DebugModuleConnectedHarts(Ranges(0 -- 4))
     ),
-    FastIntCategory(
-        Hart(Ranges(1 -- 4),
+    // TODO: puzzle out flipping Hart() and FastIntCategory
+    Hart(Ranges(1 -- 4),
+        FastIntCategory(
             FastIntMachineModeTimeRegAddr(0x1234),
             FastIntMachineModeTimeCompareRegAddr(0x1234)
         )
+    ),
+    TraceCategory(
+        TraceBranchPredictorEntries(0),
+        TraceJumpTargetCacheEntries(0),
+        TraceContextBusWidth(32)
     )
 )
 
-println("Description:")
 println(description)
 
 /*
-* category: Fast interrupt
-    * clic: 0
-        * connected harts: 1--4
-    * hart: 1--4
-        * Machine Mode Time Register Address: 0x...
-        * Machine Mode Time Compare Register Address: 0x...
-* category: Trace
-    * Number of entries in the branch predictor: 8
-    * Number of entries in the jump target cache: 2
-    * Width of context bus: 32
 * category: Physical Memory
     * tuple: LOW, HIGH addresses
         * cacheable
@@ -136,5 +138,4 @@ println(description)
         * mode restrictions
     * tuple: LOW, HIGH addresses
         * ...
-
 */
