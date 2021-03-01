771ffb9cffae440157473d3e7392c987110c17da
Martin Maas <mmaas@google.com>
2021-03-01 01:54:52 -0800

    Expand nanopb experiment.

--- binary/protobuf/nanopb/test.c
+++ binary/protobuf/nanopb/test.c
@@ -6,6 +6,15 @@
 #include "pb_decode.h"
 #include "main.pb.h"
 
+bool Configuration_decode_harts(pb_istream_t *stream, const pb_field_t *field, void **arg) {
+  Hart hart = {};
+  if (!pb_decode(stream, &Hart_msg, &hart)) {
+    return false;
+  }
+  printf("hart.mModeTimeRegAddr = %ld\n", hart.fastInt.mModeTimeRegAddr);
+  return true;
+}
+
 int main(int argc, char *argv[])
 {
     if (argc != 2) {
@@ -28,8 +37,12 @@ int main(int argc, char *argv[])
     fclose(fp);
 
     pb_istream_t stream = pb_istream_from_buffer(encoded, size);
-    Configuration config;
+    Configuration config = {};
+    config.harts.funcs.decode = Configuration_decode_harts;
+    config.harts.arg = NULL;
     pb_decode(&stream, &Configuration_msg, &config);
+
+    printf("trace.branchPredictorEntries = %ld\n", config.trace_module.branchPredictorEntries);
 
     free(encoded);
 }
