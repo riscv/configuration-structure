9b6e8246f480ab83fe90d8c488656d3ab42426b5
Tim Newsome <tim@sifive.com>
2021-09-01 11:11:12 -0700

    Run pylint on our python code.
    With multiple devs come multiple styles. Use pylint to make things a bit
    more consistent and clean.
    
    * Made schema_list local instead of global.
    * Removed try/except. If people regularly run into python stack traces,
      we can catch specific exceptions to deal with the specific cases they
      run into. Otherwise this just hides what's really going on and makes
      debugging harder.

--- asn1tools/rvcs.py
+++ asn1tools/rvcs.py
@@ -1,75 +1,81 @@
 #!/usr/bin/python3
+
+# pylint: disable=missing-function-docstring
+# pylint: disable=missing-module-docstring
+# pylint: disable=fixme
+
 import argparse
 from argparse import RawTextHelpFormatter
-import asn1tools
-import collections
 import io
 import json
 import os.path
 import sys
-import yaml
 from pprint import pprint
 
-asn1tools_formats = ("jer", "uper", "xer", "ber")
+import yaml
 
-def decode(schema, bytes, format):
-    if format == "json":
-        return json.loads(bytes)
-    elif format == "yaml":
-        stream = io.BytesIO(bytes)
+import asn1tools
+
+ASN1TOOLS_FORMATS = ("jer", "uper", "xer", "ber")
+
+def decode(schema_list, data, asn1_format):
+    if asn1_format == "json":
+        return json.loads(data)
+    if asn1_format == "yaml":
+        stream = io.BytesIO(data)
         return yaml.safe_load(stream)
-    elif format in asn1tools_formats:
-        asn1 = asn1tools.compile_files(schema_list, format)
-        return asn1.decode('Configuration', bytes)
-    else:
-        raise ValueError("Unknown format: %r" % format)
+    if asn1_format in ASN1TOOLS_FORMATS:
+        asn1 = asn1tools.compile_files(schema_list, asn1_format)
+        return asn1.decode('Configuration', data)
+    raise ValueError("Unknown format: %r" % asn1_format)
 
-def load(schema, path):
-    format = os.path.splitext(path)[1][1:]
-    bytes = open(path, "rb").read()
-    return decode(schema, bytes, format)
+def load(schema_list, path):
+    asn1_format = os.path.splitext(path)[1][1:]
+    data = open(path, "rb").read()
+    return decode(schema_list, data, asn1_format)
 
-def encode(schema, tree, format):
-    if format == "json":
+def encode(schema_list, tree, asn1_format):
+    if asn1_format == "json":
         return json.dumps(tree, indent=2).encode()
-    elif format == "yaml":
+    if asn1_format == "yaml":
         return yaml.safe_dump(tree, indent=2).encode()
-    elif format in asn1tools_formats:
-        asn1 = asn1tools.compile_files(schema_list, format)
+    if asn1_format in ASN1TOOLS_FORMATS:
+        asn1 = asn1tools.compile_files(schema_list, asn1_format)
         return asn1.encode('Configuration', tree)
-    else:
-        raise ValueError("Unknown format: %r" % format)
+    raise ValueError("Unknown format: %r" % asn1_format)
 
-def save(schema, path, tree):
-    format = os.path.splitext(path)[1][1:]
-    bytes = encode(schema, tree, format)
-    open(path, "wb").write(bytes)
+def save(schema_list, path, tree):
+    asn1_format = os.path.splitext(path)[1][1:]
+    data = encode(schema_list, tree, asn1_format)
+    open(path, "wb").write(data)
 
-def cmd_convert(args):
-    tree = load(args.schema, args.source)
+def cmd_convert(schema_list, args):
+    tree = load(schema_list, args.source)
 
     # TODO: Can we error check the tree somehow?
 
-    save(args.schema, args.destination, tree)
+    save(schema_list, args.destination, tree)
 
-def all_values(object):
+def all_values(obj):
     values = []
-    if isinstance(object, list) or isinstance(object, tuple):
-        for v in object:
-            values.extend(all_values(v))
-    elif isinstance(object, dict):
-        for k, v in object.items():
+    if isinstance(obj, (list, tuple)):
+        for value in obj:
+            values.extend(all_values(value))
+    elif isinstance(obj, dict):
+        for k, value in obj.items():
             values.extend(all_values(k))
-            values.extend(all_values(v))
+            values.extend(all_values(value))
     else:
-        values.append(object)
+        values.append(obj)
     return values
 
-def cmd_test(args):
-    import deepdiff
+def cmd_test(schema_list, args):
+    # This is not part of the standard python distribution, so only import it
+    # when we might actually use it.
+    import deepdiff # pylint: disable=import-outside-toplevel
 
     for path in args.path:
-        original = load(args.schema, path)
+        original = load(schema_list, path)
 
         if path.endswith(".jer"):
             # When loading a .jer file, asn1tools silently ignores any entries
@@ -79,8 +85,8 @@ def cmd_test(args):
         else:
             original_plain = all_values(original)
 
-        uper = encode(args.schema, original, "uper")
-        result = decode(args.schema, uper, "uper")
+        uper = encode(schema_list, original, "uper")
+        result = decode(schema_list, uper, "uper")
 
         # Check for differences between the decoded input and the result.
         difference = deepdiff.DeepDiff(original, result)
@@ -105,71 +111,64 @@ def cmd_test(args):
             return 1
 
         print("%s is %dB in UPER" % (path, len(uper)))
+    return 0
 
-def schema_path_dict(schemas_list):
+def schema_path_dict(schema_list):
     schema_dict = {}
-    for schema in schemas_list:
-        path_elements = schema.split (os.path.sep)
+    for schema in schema_list:
+        path_elements = schema.split(os.path.sep)
         if path_elements[-1].endswith(".asn"):
-            schema_dict[path_elements [-1]] = schema
+            schema_dict[path_elements[-1]] = schema
     return schema_dict
 
 def default_schema_dict():
     # Walk through ../schema for all standard ASN.1 schema files.
     default_schema_list = []
-    for root, dirs, schemafiles in os.walk(os.path.join (
-        os.path.dirname(os.path.realpath(__file__)), "..", "schema")):
+    for root, _, schemafiles in os.walk(os.path.join(
+            os.path.dirname(os.path.realpath(__file__)), "..", "schema")):
         for schemafile in schemafiles:
-             default_schema_list.append(root + os.path.sep + schemafile)
+            default_schema_list.append(root + os.path.sep + schemafile)
     return schema_path_dict(default_schema_list)
 
 def additional_schema_dict(arg_schema):
-    additional_schema_list = arg_schema.split (',')
+    additional_schema_list = arg_schema.split(',')
     verified_list = []
-    for index, additional_schema in enumerate(additional_schema_list):
+    for additional_schema in additional_schema_list:
         # Validate the schema file.
-        schema_file = os.path.normpath(os.path.join (os.path.dirname(os.path.realpath(__file__)), additional_schema))
+        schema_file = os.path.normpath(
+            os.path.join(os.path.dirname(os.path.realpath(__file__)), additional_schema))
         if os.path.exists(schema_file):
             verified_list.append(schema_file)
         else:
             print("Schema file: \"" + schema_file + "\" is not exist.")
     return schema_path_dict(verified_list)
 
-schema_help_String  = 'Specify the additional ASN.1 schema files to the standard\n' +\
+SCHEMA_HELP_STRING = 'Specify the additional ASN.1 schema files to the standard\n' +\
                       'ones under /schema directory. Multiple ASN.1 schema files\n' +\
                       'could be specified using \',\' as the separator.\n' +\
                       'Example: --schema ../myschema1.asn,../myschema2.asn or\n'+\
                       '         --schema absolute_dir/myschema3.asn\n'
-convert_help_String = 'Convert between binary and human-readable formats.'
-test_help_String    = 'Test encoding/decoding the given files.'
+CONVERT_HELP_STRING = 'Convert between binary and human-readable formats.'
+TEST_HELP_STRING = 'Test encoding/decoding the given files.'
 
-schema_list = []
 def main():
-    
     # Build up the default schema dictionary.
-    try:
-        default_schema = default_schema_dict()
-        if len (default_schema) == 0:
-            print("No default schemas found.")
-            sys.exit ()
-    except:
-        print("Can't build up the default schema dictionary.")
-        sys.exit ()
+    default_schema = default_schema_dict()
+    if len(default_schema) == 0:
+        print("No default schemas found.")
+        return 1
 
     parser = argparse.ArgumentParser(formatter_class=RawTextHelpFormatter)
-    parser.add_argument('--schema',
-            help=schema_help_String)
+    parser.add_argument('--schema', help=SCHEMA_HELP_STRING)
 
     subparsers = parser.add_subparsers()
 
-    parse_convert = subparsers.add_parser('convert',
-            help=convert_help_String)
+    parse_convert = subparsers.add_parser('convert', help=CONVERT_HELP_STRING)
     parse_convert.add_argument('source')
     parse_convert.add_argument('destination')
     parse_convert.set_defaults(func=cmd_convert)
 
-    parse_test = subparsers.add_parser('test',
-            help=test_help_String)
+    parse_test = subparsers.add_parser('test', help=TEST_HELP_STRING)
     parse_test.add_argument('path', nargs='+')
     parse_test.set_defaults(func=cmd_test)
 
@@ -178,28 +177,22 @@ def main():
     if args.schema:
         # Build up the dictionary of schemas assigned against to --schema option.
         schema_dictionary = {}
-        try:
-            # Generate the dictionary of additional schemas.
-            schema_dictionary = additional_schema_dict(args.schema)
-            if len(schema_dictionary) == 0:
-                print("Can't find the schemas assigned to --schema option.")
-                return 1
-            else:
-                # Combine schema_dictionary with the default schema.
-                for default_schema_name in default_schema:
-                    if default_schema_name not in schema_dictionary:
-                        schema_dictionary[default_schema_name] = default_schema[default_schema_name]
-        except:
-            print("Fail to build up the dictionary of schemas assigned to --schema option.")
+
+        # Generate the dictionary of additional schemas.
+        schema_dictionary = additional_schema_dict(args.schema)
+        if len(schema_dictionary) == 0:
+            print("Can't find the schemas assigned to --schema option.")
             return 1
+        # Combine schema_dictionary with the default schema.
+        for default_schema_name in default_schema:
+            if default_schema_name not in schema_dictionary:
+                schema_dictionary[default_schema_name] = default_schema[default_schema_name]
     else:
         schema_dictionary = default_schema
 
-    # Convert schema dictionary to a list.
-    for schema in schema_dictionary:
-        schema_list.append (schema_dictionary[schema])
+    schema_list = list(schema_dictionary.values())
 
-    return args.func(args)
+    return args.func(schema_list, args)
 
 if __name__ == '__main__':
     sys.exit(main())
